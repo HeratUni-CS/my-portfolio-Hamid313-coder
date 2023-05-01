e36cacee031c7cd6a042b185bdb0b216ec04f795
hamid313-coder <hamid.hussainy0@gmail.com>
2023-05-01 14:53:34 +0430

    created portfolio

--- lib/main.dart
+++ lib/main.dart
@@ -14,54 +14,104 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(56),
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(24),
+            decoration: const BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                CircleAvatar(
+                  foregroundImage: AssetImage("assets/images/me.jpg"),
+                  radius: 56,
+                ),
+                Text(
+                  "Hamid Hussainy",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 102, 245, 231),
+                      fontSize: 38,
+                      fontFamily: "pacific"),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 102, 245, 231),
+                      fontSize: 28,
+                      fontFamily: "ubuntu"),
+                ),
+                Divider(
+                  color: Color.fromARGB(255, 102, 245, 231),
+                ),
+                MyTile(
+                  icon: Icon(Icons.link),
+                  title: "Website",
+                ),
+                MyTile(
+                  icon: Icon(Icons.email),
+                  title: "Email",
+                ),
+                MyTile(
+                  icon: Icon(Icons.flutter_dash),
+                  title: "Twitter",
+                ),
+                MyTile(
+                  icon: Icon(Icons.facebook),
+                  title: "Facebook",
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class MyTile extends StatelessWidget {
+  const MyTile({
+    Key? key,
+    required this.icon,
+    required this.title,
+  }) : super(key: key);
+
+  final Widget icon;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(80), color: Colors.white),
+      child: ListTile(
+          leading: icon,
+          title: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.teal,
+              fontSize: 16,
+            ),
+          )),
     );
   }
 }
